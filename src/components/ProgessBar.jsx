45a0115b641b29f5bda9d4f0b4f3acf52447cabc
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-03-18 09:55:39 +0530

    first commit

---
+++ src/components/ProgessBar.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+export default function ProgessBar({ timer }) {
+
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log(remainingTime)
+            setRemainingTime((prevTime) => prevTime - 10)
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+    return (
+        <progress value={remainingTime} max={timer} />
+    )
+}
